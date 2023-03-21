96e883b68da63d389d3c4552ac970f131d696b67
Nabeela-Maham <nabeela.maham@stud.fra-uas.de>
2023-03-21 16:44:24 -0700

    Scaffold AppBar widget

---
+++ scoffold_appbar_widgets/android/app/src/main/java/com/example/scoffold_appbar_widgets/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.scoffold_appbar_widgets;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ scoffold_appbar_widgets/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/material/floating_action_button.dart';
+
+void main() => runApp(MaterialApp(
+  //home: Text('Hey Nabeela'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+        'hello Nabeela',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      )
+    ),
+  floatingActionButton: FloatingActionButton(
+    onPressed: () {},
+    child: Text('Click'),
+    backgroundColor: Colors.red[600],
+  ),
+
+  )
+
+));
