9c689656041180d68d84789214c149d4df3c4878
Nabeela-Maham <nabeela.maham@stud.fra-uas.de>
2023-03-23 12:47:09 -0700

    scoffold appbar widgets

--- scoffold_appbar_widgets/lib/main.dart
+++ scoffold_appbar_widgets/lib/main.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/material/floating_action_button.dart';
+//import 'package:flutter/src/material/floating_action_button.dart';
 
 void main() => runApp(MaterialApp(
-  //home: Text('Hey Nabeela'),
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        'hello Nabeela',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
+    //home: Text('Hey Nabeela'),
+    home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'hello Nabeela',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
         ),
-      )
-    ),
-  floatingActionButton: FloatingActionButton(
-    onPressed: () {},
-    child: Text('Click'),
-    backgroundColor: Colors.red[600],
-  ),
-
-  )
-
-));
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
