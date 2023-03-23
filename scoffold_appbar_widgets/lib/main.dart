a1fa3643907cd0c7d24ab0a2fe6e3e064bd9f141
Nabeela-Maham <nabeela.maham@stud.fra-uas.de>
2023-03-23 13:36:07 -0700

    Images

--- scoffold_appbar_widgets/lib/main.dart
+++ scoffold_appbar_widgets/lib/main.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/src/material/floating_action_button.dart';
+import 'package:flutter/src/material/floating_action_button.dart';
 
 void main() => runApp(MaterialApp(
-    //home: Text('Hey Nabeela'),
-    home: Home()));
+  //home: Text('Hey Nabeela'),
+  home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('my first app'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
+      title: Text('my first app'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image(
-          image: NetworkImage(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLmkwYbPm-neI63boKZ1YJ81175mtbxOBdwA&usqp=CAU'),
-        ),
-      ),
+      child: Image.asset("assets/space-1.jpg"),
+
+    /* Image(
+
+        image: AssetImage('assets/space-1.jpeg'),
+        image: NetworkImage(
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLmkwYbPm-neI63boKZ1YJ81175mtbxOBdwA&usqp=CAU'),
+    ) */ ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Click'),
-        backgroundColor: Colors.red[600],
-      ),
-    );
+      onPressed: () {},
+      child: Text('Click'),
+      backgroundColor: Colors.red[600],
+      )
+
+      );
+
   }
+
 }
