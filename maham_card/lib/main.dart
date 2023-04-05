4178ab9c9df887d33e2e7e77523d45fc94b5f0b3
Nabeela-Maham <nabeela.maham@stud.fra-uas.de>
2023-04-05 15:05:21 -0700

    Maham Card using statefulWidgets and increment on floating window.

--- maham_card/lib/main.dart
+++ maham_card/lib/main.dart
@@ -5,88 +5,109 @@ void main() => runApp(const MaterialApp(
     home: MahamCard(),
 ));
 
-class MahamCard extends StatelessWidget {
+
+class MahamCard extends StatefulWidget{
   const MahamCard({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  _MahamCardState createState() => _MahamCardState();
+}
+int MahamLevel=0;
+class _MahamCardState extends State<MahamCard>
+    {
+      @override
+    Widget build(BuildContext context){
+        return Scaffold(
         appBar: AppBar(
-          title: const Text('Maham ID Card'),
-          centerTitle: true,
-          backgroundColor: Colors.grey[300],
-       elevation: 0.0,
+        title: const Text('Maham ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[300],
+        elevation: 0.0,
         ),
-      body: Padding(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              MahamLevel +=1;
+            });
+          },
+
+          child: Icon(Icons.add),
+          backgroundColor: Colors.grey[800],
+        ),
+
+        body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
 
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/maham.jpeg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[400],
-            ),
-            Text('NAME',
-              style: TextStyle(
-              color: Colors.grey[400],
-              letterSpacing: 2.0,
-            ),
-            ),
-            Text('NABEELA',
-            style: TextStyle(
-              color: Colors.pinkAccent,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            SizedBox(height: 30.0),
-
-
-            Text('CURRENT MAHAM LEVEL',
-              style: TextStyle(
-                color: Colors.grey[400],
-                letterSpacing: 2.0,
-              ),
-            ),
-            Text('8',
-              style: TextStyle(
-                color: Colors.pinkAccent,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-
-
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.pinkAccent,
-
-                ),
-                SizedBox(width: 10.0,),
-                Text(
-                  'mahamnabila@gmail.com',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                )
-              ],
-            )
-          ],
+        children: <Widget>[
+        Center(
+        child: CircleAvatar(
+        backgroundImage: AssetImage('assets/maham.jpeg'),
+        radius: 40.0,
         ),
-      ),
-       );
-  }
-}
+        ),
+        Divider(
+        height: 60.0,
+    color: Colors.grey[400],
+    ),
+    Text('NAME',
+    style: TextStyle(
+    color: Colors.grey[400],
+    letterSpacing: 2.0,
+    ),
+    ),
+    Text('NABEELA',
+    style: TextStyle(
+    color: Colors.pinkAccent,
+    letterSpacing: 2.0,
+    fontSize: 28.0,
+    fontWeight: FontWeight.bold,
+    ),
+    ),
+    SizedBox(height: 30.0),
+
+
+    Text('CURRENT MAHAM LEVEL',
+    style: TextStyle(
+    color: Colors.grey[400],
+    letterSpacing: 2.0,
+    ),
+    ),
+    Text('$MahamLevel',
+    style: TextStyle(
+    color: Colors.pinkAccent,
+    letterSpacing: 2.0,
+    fontSize: 28.0,
+    fontWeight: FontWeight.bold,
+    ),
+    ),
+    SizedBox(height: 30.0),
+
+
+    Row(
+    children: <Widget>[
+    Icon(
+    Icons.email,
+    color: Colors.pinkAccent,
+
+    ),
+    SizedBox(width: 10.0,),
+    Text(
+    'mahamnabila@gmail.com',
+    style: TextStyle(
+    color: Colors.grey[400],
+    fontSize: 18.0,
+    letterSpacing: 1.0,
+    ),
+    )
+    ],
+    )
+    ],
+    ),
+    ),
+    );
+    }
+    }
+
+
