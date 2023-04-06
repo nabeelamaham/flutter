c66bc3c59e8cdded9b012012321e5328508ac52f
Nabeela-Maham <nabeela.maham@stud.fra-uas.de>
2023-04-06 04:54:15 -0700

    Alternate method used to display data in the list(All methods are written in the code)

--- list_data/lib/main.dart
+++ list_data/lib/main.dart
@@ -15,11 +15,20 @@ class QuoteList extends StatefulWidget{
 int MahamLevel=0;
 class _QuoteListState extends State<QuoteList>
 {
+  //Method 1
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Today is hard'),
+
+    Quote(author: 'Oscar Wilde', text: 'tomorrow will be worse'),
+    Quote(author: 'Oscar Wilde', text: 'but the day after tomorrow will be sunshine')
+  ];
+  //Method 1
+  /*
   List<String> quotes =[
     'Today is hard',
     'tomorrow will be worse',
     'but the day after tomorrow will be sunshine'
-  ];
+  ];*/
 
   @override
   Widget build(BuildContext context){
@@ -33,11 +42,16 @@ class _QuoteListState extends State<QuoteList>
 
       body:Column(
         //method 1
-        children: quotes.map((quote) {
-          return Text(quote);}
-    ).toList(),
+     //   children: quotes.map((quote) {
+       //   return Text(quote);}
+//    ).toList(),
 //method 2
-        //children: quotes.map((quote) => Text(quote)).toList(),
+      //method 1
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+
+        //method 2
+      //  children: quotes.map((quote) => Text('$quote')).toList(),
+
       ) ,
     );
   }
